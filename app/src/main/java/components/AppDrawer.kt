5039433099f2a72478dfa66ca5670f3b8d523639
Megadaytedenegbite <68592612+Megadaytedenegbite@users.noreply.github.com>
2024-05-07 23:17:24 +0300

    Изменения в AppDrawer.kt: Добавлен переключатель темы со светлой на темную.

--- app/src/main/java/components/AppDrawer.kt
+++ app/src/main/java/components/AppDrawer.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -65,7 +66,7 @@ private fun ScreenNavigationButton(
 ) {
     val colors = MaterialTheme.colors
 
-    val imageVector = if (isSelected) {
+    val imageAlpha = if (isSelected) {
         1f
     } else {
         0.6f
@@ -100,7 +101,8 @@ private fun ScreenNavigationButton(
             Image(
                 imageVector = icon,
                 contentDescription = "Screen Navigation Button",
-                colorFilter = ColorFilter.tint(textColor)
+                colorFilter = ColorFilter.tint(textColor),
+                alpha = imageAlpha
             )
             Spacer(Modifier.width(16.dp))
             Text(
@@ -110,5 +112,37 @@ private fun ScreenNavigationButton(
                 modifier = Modifier.fillMaxWidth()
             )
         }
+    }
+}
+
+@Composable
+private fun LightDarkThemeItem() {
+    Row(
+        Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = "Включить темную тему",
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+        Switch(
+            checked = NotesThemeSettings.isDarkThemeEnabled,
+            onCheckedChange = { NotesThemeSettings.isDarkThemeEnabled = it },
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LightDarkThemeItemPreview() {
+    NotesTheme {
+        LightDarkThemeItem()
     }
 }
