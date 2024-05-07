312b0954565a521d94048c34ff73aaa7c1a1e0d3
Megadaytedenegbite <68592612+Megadaytedenegbite@users.noreply.github.com>
2024-05-07 23:36:20 +0300

    Созданы пакет Screens и файл SaveNoteScreen.kt. В SaveNoteScreen.kt добавлен дизайн для дополнительного функционала и лист цветов. Изменения в Note.kt: Добавлена стилизация текста.

--- app/src/main/java/components/Note.kt
+++ app/src/main/java/components/Note.kt
@@ -2,23 +2,39 @@ package com.topic2.android.notes.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material.Checkbox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
+import components.NoteColor
 
 @Composable fun Note() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+    ) {
+        NoteColor(
+            color = rwGreen,
+            size =  40.dp,
+            padding = 4.dp,
+            border = 1.dp
         )
-        Text(text = "Заголовок", maxLines = 1)
-        Text(text = "Содержимое", maxLines = 1)
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Заголовок", maxLines = 1)
+            Text(text = "Содержимое", maxLines = 1)
+        }
         Checkbox(
             checked = false, onCheckedChange = { },
             modifier = Modifier.padding(start = 8.dp)
---
+++ app/src/main/java/components/NoteColor.kt
@@ -0,0 +1,50 @@
+package components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteColor(
+    modifier: Modifier,
+    color: Color,
+    size: Dp,
+    border: Dp,
+    padding: Dp
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(color)
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+                ),
+                CircleShape
+            )
+    )
+}
+
+@Preview @Composable
+fun NoteColorPreview() {
+    NoteColor(
+        modifier = Modifier,
+        color = Color.Red,
+        size = 40.dp,
+        border = 2.dp,
+        padding = 4.dp
+    )
+}
