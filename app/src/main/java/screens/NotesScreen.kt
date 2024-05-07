48b97275f8468789c9561aa69c7dd0d91b1e35c6
Megadaytedenegbite <68592612+Megadaytedenegbite@users.noreply.github.com>
2024-05-08 00:33:22 +0300

    Добавлен файл TrashScreen.kt в screens, заменен файл MainViewMode.kt  в viewmodel
    Изменения в NotesScreen.kt:
    Добавлена панель приложения и плавующая кнопка.

--- app/src/main/java/screens/NotesScreen.kt
+++ app/src/main/java/screens/NotesScreen.kt
@@ -1,17 +1,29 @@
 package com.topic2.android.notes.ui.components.screens
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.ui.components.AppDrawer
 import com.topic2.android.notes.ui.components.Note
 import com.topic2.android.notes.ui.components.TopAppBar
 import com.topic2.android.notes.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun NotesScreen(viewModel: MainViewModel) {
@@ -19,18 +31,52 @@ fun NotesScreen(viewModel: MainViewModel) {
         .notesNotInTrash
         .observeAsState(listOf())
 
-    Column {
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+
+    Scaffold(topBar = {
         TopAppBar(
-            title =  "Заметки",
+            title = "Notes",
             icon = Icons.Filled.List,
             onIconClick = {}
         )
-        NotesList(
-            notes = notes,
-            onNoteCheckedChange = { viewModel.onNoteCheckedChange(it) },
-            onNoteClick = { viewModel.onNoteClick(it)}
-        )
-    }
+    },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            AppDrawer(
+                currentScreen = Screen.Notes,
+                closeDrawerAction = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                }
+            )
+        },
+        content = {
+            if (notes.isNotEmpty()) {
+                NotesList(
+                    notes = notes,
+                    onNoteCheckedChange = {
+                        viewModel.onNoteCheckedChange(it)
+                    },
+                    onNoteClick = {viewModel.onNoteClick(it)}
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { viewModel.onCreateNewNoteClick()},
+                contentColor = MaterialTheme.colors.background,
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add Note Button"
+                    )
+                }
+            )
+        }
+    )
 }
 
 @Composable
