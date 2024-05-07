0aa3bf1043bc3740c233c5bee341c6fdc6fb79ba
Megadaytedenegbite <68592612+Megadaytedenegbite@users.noreply.github.com>
2024-05-07 23:13:52 +0300

    Добавлены пакет ui.components и файл Note.kt. В Note.kt добавлены составные элементы для предоставления цветного виджета, заголовка. содержимого и флажка.

---
+++ app/src/main/java/components/Note.kt
@@ -0,0 +1,30 @@
+
+package com.topic2.android.notes.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.rwGreen
+
+@Composable fun Note() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(rwGreen)
+        )
+        Text(text = "Заголовок", maxLines = 1)
+        Text(text = "Содержимое", maxLines = 1)
+        Checkbox(
+            checked = false, onCheckedChange = { },
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable @Preview private fun NotePreview() { Note() }
